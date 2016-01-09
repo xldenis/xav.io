1abeecfbd99f1cd87cc1c39eb37ed0bdd36d22ee
Xavier Denis <xldenis@gmail.com>
2016-01-08 19:39:13 -0600

    Add syntax highlighting to command.

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,32 +1,54 @@
 
 //Prints out text one letter at a time with delay.
-function type(elem,text,delay,callback) {
-  function next(i) {
-    var randDelay = Math.floor(Math.random() * 100) - 40;
-    elem.innerHTML = text.substr(0,i)+"▊";
-    if(text.length >= i){
-      setTimeout(function() {next(i+1)} ,delay + randDelay);
-    }else {
-      elem.innerHTML = text.substr(0,i);
-      if(callback != null) {
-        callback();
-      }
-    }
+function type(elem, text, delay, callback) {
+  for(var eId = 0; eId < elem.children.length; eId++){
+    elem.children[eId].style.opacity = 0;
   }
-  next(0);
+
+  function next(remText, elemId) {
+
+    if(remText == ""){
+      var child = elem.children[elemId];
+
+      if(elemId > 0) {
+        elem.children[elemId-1].innerHTML = elem.children[elemId-1].innerHTML.slice(0,-1);
+      }
+
+      if(!(elemId < elem.children.length)) {
+        return callback();
+      }
+
+      remText = child.innerHTML.trim();
+      child.innerHTML = "▊";
+      child.style.opacity = 1;
+
+      elemId += 1;
+
+    }
+
+    var child = elem.children[elemId-1];
+
+    child.innerHTML = child.innerHTML.slice(0,-1) + remText[0]+"▊";
+    remText = remText.slice(1);
+
+    var randDelay = Math.floor(Math.random() * 100) - 40;
+
+    setTimeout(function() {next(remText, elemId)}, delay + randDelay);
+  }
+  next("",0);
 
 }
 
 function load(elem,delay) {
   setTimeout(function() {
     function show(i){
-    if(elem.childElementCount >= i){
-      elem.children[i].style.opacity = 1;
-      setTimeout(function() {show(i+1)} , 25);
+      if(elem.childElementCount >= i){
+        elem.children[i].style.opacity = 1;
+        setTimeout(function() {show(i+1)} , 25);
+      }
     }
-  }
-  show(0);
-  elem.style.opacity = 1;
+    show(0);
+    elem.style.opacity = 1;
   },delay);
 }
 
@@ -41,8 +63,8 @@ function request(com,resp,typeSpeed,lag) {
 }
 
 window.onload = function () {
-  el = document.getElementsByClassName("command")[0];
+  el = document.getElementsByClassName("zsh")[0];
   el2 = document.getElementsByClassName("hash")[0];
-  typeDelay = 50
-  request(el, el2, typeDelay, 500);
+  typeDelay = 50;
+  request(el, el2, typeDelay, 600);
 }
